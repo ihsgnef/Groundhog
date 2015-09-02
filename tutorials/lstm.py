ff7aa5ea98c51f88a4a7c55b8fb616a4924b825f
sjtufs <sjtu_fs@outlook.com>
2015-09-02 15:00:30 +0800

    Adapt to new version of theano. No more theano.sandbox.scan

--- tutorials/lstm.py
+++ tutorials/lstm.py
@@ -31,7 +31,7 @@ from groundhog.layers import
         hard_sigmoid, \
         hard_tanh
 from groundhog.models import LM_Model
-from theano.sandbox.scan import scan
+from theano import scan
 
 import numpy as np
 import theano
