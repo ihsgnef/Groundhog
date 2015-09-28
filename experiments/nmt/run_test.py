0f9af5a4adb1980ee6ba3525b01de3e34cc35b31
sjtufs <sjtu_fs@outlook.com>
2015-09-28 19:09:19 +0800

    update test

--- experiments/nmt/run_test.py
+++ experiments/nmt/run_test.py
@@ -1,5 +1,6 @@
 import os
 
-cmd = 'python sample.py --source=en.short --beam-search --beam-size 60 --trans trans.txt --state models/search_state_fr-en.pkl models/search_model_fr-en.npz 2>>log.txt'
+cmd = 'python sample.py --source=fr.short --beam-search --beam-size 60 --trans trans.txt --state models/search_state_fr-en.pkl models/search_model_fr-en.npz 2>>log.txt'
+# cmd = 'python sample.py --source=fr.short --beam-search --beam-size 60 --trans trans.txt --state search_state.pkl search_model.npz 2>>log.txt'
 
 os.system(cmd)
